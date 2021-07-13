11f0f33361b35c1d83dd2ce0a2bd069f681239dd
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-07-13 20:26:23 +0300

    add good fail for test on isosceles

---
+++ TriangleTracker.Tests/ModelTests/TriangleTests.cs
@@ -0,0 +1,19 @@
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriangleTracker;
+
+
+namespace TriangleTracker.Tests
+{
+  [TestClass]
+  public class TriangleTests
+  { 
+  [TestMethod]
+public void CheckTri_ChecksTypeOfTriangle_Isoceles()
+{
+  Triangle testTriangle = new Triangle(3,3,5);
+  Assert.AreEqual("Isoceles", testTriangle.CheckTri());
+}
+
+   }
+}
