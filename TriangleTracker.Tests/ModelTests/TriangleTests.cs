1c2cfc8224d623f90ebd63621ed41cc6ce930226
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-07-13 21:51:14 +0300

    add a passed test for Not a Triangle triangle

--- TriangleTracker.Tests/ModelTests/TriangleTests.cs
+++ TriangleTracker.Tests/ModelTests/TriangleTests.cs
@@ -21,10 +21,18 @@ public void CheckTri_CheckTypeTriangle_Equilateral()
   Triangle testTriangle = new Triangle(2, 2, 2);
   Assert.AreEqual("Equilateral", testTriangle.CheckTri());
 }
+
+[TestMethod]
+public void CheckTri_CheckTypeTriangle_NotATriangle()
+{
+  Triangle testTriangle = new Triangle(4, 6, 14);
+  Assert.AreEqual("Not a Triangle", testTriangle.CheckTri());
+}
+
 [TestMethod]
 public void CheckTri_CheckTypeTriangle_Scalene()
 {
-  Triangle testTriangle = new Triangle(2, 3, 4);
+  Triangle testTriangle = new Triangle(3, 5, 4);
   Assert.AreEqual("Scalene", testTriangle.CheckTri());
 }
 }
